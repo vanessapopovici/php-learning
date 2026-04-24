4c74b1ded6f65969df7cc81142b5076fca06e5a4
vanessapopovici <vanessamariapopovici@gmail.com>
2026-04-24 21:38:13 +0300

    Learned PDO: Connecting to MySQL and fetching posts

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
 require ('functions.php');
-require ('router.php');
+//require ('router.php');
+
+//Conect to database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
